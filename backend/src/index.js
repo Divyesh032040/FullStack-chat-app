7094075addbb20edce7b4aa4a6f703ea9787c5d2
Divyesh Parmar <dp032040@gmail.com>
2025-03-24 12:22:04 +0530

    update firname

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./lib/db.js";
 import {app , server } from "./lib/socket.js"
+import { fileURLToPath } from 'url';
 import path from 'path';
 
 dotenv.config();
@@ -33,6 +34,9 @@ import messageRoute  from "./routes/message.route.js"
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
