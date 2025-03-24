fbfb074af6afd3d4092ea858071d11b5edc33f14
Divyesh Parmar <dp032040@gmail.com>
2025-03-24 12:33:05 +0530

    update path

--- backend/src/index.js
+++ backend/src/index.js
@@ -39,7 +39,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
   
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
